c384052ad18fc7745d2d395930cd3f1b69864387
Micah Shute <micah.shute@gmail.com>
2018-05-14 20:44:29 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,11 +1,18 @@
-$content = Array.new(9, " ")
+content = Array.new(9, " ")
 
-def create_row(x)
+def create_row(x, content)
   pos = (x - 1) * 3
-  spaceContent($content[pos]) + '|' + spaceContent($content[pos + 1]) + '|' + spaceContent($content[pos + 2])
+  spaceContent(content[pos]) + '|' + spaceContent(content[pos + 1]) + '|' + spaceContent(content[pos + 2]) + "\n"
 end
 
 def spaceContent(content)
   " #{content} "
 end
 
+def create_divider()
+  "-----------" + "\n"
+end
+
+def create_board(content)
+  create_row(0, content) + create_divider + create_row(1, content) + create_divider + create_row(2, content)
+end
