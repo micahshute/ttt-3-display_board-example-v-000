c1e39fbb826aae6d28cae5dea8a89fa27b1129e3
Micah Shute <micah.shute@gmail.com>
2018-05-14 20:48:02 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,4 +1,4 @@
-content = Array.new(9, " ")
+$content = Array.new(9, " ")
 
 def create_row(x, content)
   pos = (x - 1) * 3
@@ -17,4 +17,6 @@ def create_board(content)
   create_row(0, content) + create_divider + create_row(1, content) + create_divider + create_row(2, content)
 end
 
-puts(create_board(content))
+def display_board()
+  puts(create_board($content))
+end
